680778ac4ae0dfd90bf9884bd6a0acbf1767aa45
sunchengchhay <sonchhay2018@gmail.com>
2024-01-15 22:26:38 +0700

    first commit

---
+++ Filters/ExceptionFilters/Shirt_HandleUpdateExceptionsFilterAttribute.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using WebAPIDemo.Models.Respositories;
+
+namespace WebAPIDemo.Filters
+{
+    public class Shirt_HandleUpdateExceptionsFilterAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            base.OnException(context);
+
+            var strShirtId = context.RouteData.Values["id"] as string;
+            if (int.TryParse(strShirtId, out int shirtId))
+            {
+                if (!ShirtRepository.ShirtExists(shirtId))
+                {
+                    context.ModelState.AddModelError("ShirtId", "Shirt doesn't exist anymore.");
+                    var problemDetails = new ValidationProblemDetails(context.ModelState)
+                    {
+                        Status = StatusCodes.Status400BadRequest
+                    };
+                    context.Result = new BadRequestObjectResult(problemDetails);
+                }
+            }
+        }
+    }
+}
